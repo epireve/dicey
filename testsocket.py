be9a57ba8fef43add56d6c87262a4842b855cb4f
Firdaus Adib <i@firdaus.my>
2018-11-16 17:00:47 +0800

    Array format

--- testsocket.py
+++ testsocket.py
@@ -1,4 +1,5 @@
 import websocket
+import warnings; warnings.simplefilter('ignore')
 import time
 import json
 
@@ -11,9 +12,13 @@ def on_message(ws, message):
     # print(message)
     striped = message.lstrip("42[\"betReceipt\",").rstrip("]")
     iteration = json.loads(striped)
-    print(json.dumps(iteration))
+    data = json.dumps(iteration)
+    # print(json.dumps(iteration))
+    message_list = iteration["payload"]["receipt"]
+    print(message_list)
+    # print(data)
     # for key, value in dict.items(iteration["payload"]["receipt"]):
-    #     print(key,":",value, end=' ')
+    #     print(key,":",value, end=' |', flush=True)
     # print("\n")
 
 def on_error(ws, error):
@@ -25,9 +30,9 @@ def on_close(ws):
 def on_open(ws):
     def run(*args):
         for i in range(10):
-            time.sleep()
+            time.sleep(600)
             ws.send("Hello %d" % i)
-        time.sleep()
+        time.sleep(600)
         ws.close()
         print("thread terminating...")
     thread.start_new_thread(run, ())
@@ -36,8 +41,9 @@ def on_open(ws):
 if __name__ == "__main__":
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("wss://betdice.one/dice/prod/ws/?EIO=3&transport=websocket",
+                              on_open = on_open,
                               on_message = on_message,
-                              # on_error = on_error,
+                              on_error = on_error,
                               on_close = on_close)
     # ws.on_open = on_open
     ws.run_forever()
